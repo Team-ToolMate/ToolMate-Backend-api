268ba33997287d78af756c19a6883bc89e0d6db7
Thinuka-Perera <thinukapereraa@gmail.com>
2025-12-10 17:04:50 +0530

    small Update

--- src/main/java/com/toolmate/toolmate_api/ToolmateApiApplication.java
+++ src/main/java/com/toolmate/toolmate_api/ToolmateApiApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ToolmateApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ToolmateApiApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(ToolmateApiApplication.class, args);
+    }
 }
